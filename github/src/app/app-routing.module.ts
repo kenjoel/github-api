41e570d8a89ea093478dc8881f3c526670d218ba
Ken-muigai <kenmuigai980@gmail.com>
2020-08-31 13:28:01 +0300

    WIP

--- github/src/app/app-routing.module.ts
+++ github/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
+import { LandingComponent } from "./landing/landing.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"profile", component:UsersComponent
+  },
+  {
+    path:"repos", component:ReposComponent
+  },
+  {
+    path:"", redirectTo:"/profile", pathMatch:"full"
+  },
+  {
+    path:"**", component:LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
