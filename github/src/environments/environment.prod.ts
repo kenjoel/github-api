4e3bc4d41d8259eaa2ec327c019775f78a523088
Ken-muigai <kenmuigai980@gmail.com>
2020-08-31 14:15:00 +0300

    WC

--- github/src/environments/environment.prod.ts
+++ github/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  ApiUrl: "https://api.github.com/users/",
+  clientId:"9a67142bcc620287a7cf",
+  accessToken:"0ffac34a78bf444a3e04efbcdddcb4270acd54c8"
 };
